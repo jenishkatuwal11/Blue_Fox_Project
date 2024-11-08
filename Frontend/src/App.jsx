7a78ce85558e7fbd5fd3f65b5940601b6c0d33f1
PseudoCoder11 <jenishkatuwal7@gmail.com>
2024-11-08 08:46:05 +0545

    change in app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landingpage from "./pages/HomePage/Landingpage";
 import FindRoom from "./pages/FindRoom/FindRoom";
